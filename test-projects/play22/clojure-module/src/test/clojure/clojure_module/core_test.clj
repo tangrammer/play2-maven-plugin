bb196b327a8cea50fa5f09e6895a2717f32d2d6a
tangrammer <tangrammer@tangrammers-MacBook-Pro.local>
2015-04-09 13:07:53 +0200

    Add clojure module with zi plugin

---
+++ test-projects/play22/clojure-module/src/test/clojure/clojure_module/core_test.clj
@@ -0,0 +1,7 @@
+(ns clojure-module.core-test
+  (:require [clojure.test :refer :all]
+            [clojure-module.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
